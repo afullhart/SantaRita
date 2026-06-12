eef9c1232df97e2c47d71b4b950e50406d353e3f
afullhart <58483164+afullhart@users.noreply.github.com>
2026-06-11 22:17:24 -0700

    Update DEM export description and scale

--- GEE/Export_30m_DEM.js
+++ GEE/Export_30m_DEM.js
@@ -38,7 +38,7 @@ Map.addLayer(hillshade_check, {min: 0, max: 255}, 'Stripe-Free Asset Hillshade P
 // =========================================================================
 Export.image.toAsset({
   image: dem_30m_resampled.rename('elevation'),
-  description: 'SR_30m_DEM_Pristine',
+  description: 'SR_30m_DEM_Resampled',
   assetId: 'projects/ee-andrewfullhart/assets/SR_30m_DEM_Resampled', // Updated Asset ID
   region: bounds_geom,
   scale: 30, // THE CRITICAL FIX: Changed from 10 to 30
