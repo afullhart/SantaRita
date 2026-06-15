c6aae57f0c2a5d63c1b46ffe4f4469b53efaf9df
afullhart <58483164+afullhart@users.noreply.github.com>
2026-06-15 16:06:37 -0700

    Update Model_Visualization.js

--- GEE/Model_Visualization.js
+++ GEE/Model_Visualization.js
@@ -15,7 +15,7 @@ var sep_end   = '2019-09-20';
 // =========================================================================
 var fc = ee.FeatureCollection('projects/ee-andrewfullhart/assets/SR_s2_model_grid_utm');
 var bounds_fc = ee.FeatureCollection('projects/ee-andrewfullhart/assets/SR_bounds');
-var cloud_windows = ee.FeatureCollection('projects/ee-andrewfullhart/assets/Cloud_FeatureClass');
+var cloud_windows = ee.FeatureCollection('projects/ee-andrewfullhart/assets/Cloud_FeatureClass_S2');
 var bounds_geom = bounds_fc.first().geometry().bounds();
 
 // =========================================================================
